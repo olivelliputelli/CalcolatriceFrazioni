44278450fb3353f8ad221915b005c334759add4e
Davide Primino <dprimino@olivelliputelli.it>
2022-02-21 13:01:43 +0100

    Operatori overload

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,7 +3,7 @@
 Frazione f1 = new Frazione("2 / 3"); // 12/8
 Frazione f2 = new Frazione("3"); // 2/6
 
-
+// 2/3 -> 3/2
 Frazione f5 = Frazione.Reciproca(f1);
 
 Console.WriteLine($"{f5.Numeratore}/{f5.Denominatore}"); // 11/6
