2bb1d4f2dd87b0e38cea39772f882a1e1a8f3ca2
Davide Primino <dprimino@olivelliputelli.it>
2022-03-18 13:51:53 +0100

    Alfabeto

--- ClassiUtili/GeneratorePassword.cs
+++ ClassiUtili/GeneratorePassword.cs
@@ -8,12 +8,61 @@
     /// <seealso href="https://youtu.be/n6R2AE3Htj0"/>
     public class GeneratorePassword
     {
-        public int LunghezzaMinima { get; set; } = 3;
+        private static readonly Random g = new Random();
+        private string alfabeto = "0123456789";
+        public string Alfabeto
+        {
+            get => alfabeto;
+            set
+            {
+                if (value.Length == 0)
+                    throw new GeneratorePasswordException("Almeno un carattere.");
+                else
+                    alfabeto = value;
+            }
+        }
+        private int lunghezzaMinima = 3;
+        public int LunghezzaMinima
+        {
+            get => lunghezzaMinima;
+            set
+            {
+                if (value < 3)
+                    throw new GeneratorePasswordException("Lunghezza minima 3 simboli.");
+                else
+                    lunghezzaMinima = value;
+            }
+        }
+
+        public GeneratorePassword(TipoPassword tipoPassword)
+        {
+            if (tipoPassword == TipoPassword.Lettere)
+                for (char i = 'A'; i <= 'Z'; i++) Alfabeto += $"{(char)i}{(char)(i + 32)}";
+            else if (tipoPassword == TipoPassword.CaratteriSpeciali)
+            {
+                alfabeto = "";
+                for (int i = 32; i <= 126; i++) Alfabeto += $"{(char)i}";
+            }
+
+        }
 
         public string NuovaPassword(int lunghezza)
         {
-            // Da fare..
-            return "Ciao";
+            if (lunghezza < LunghezzaMinima)
+                throw new GeneratorePasswordException("Password troppo corta.");
+            string psw = "";
+            for (int i = 0; i < lunghezza; i++)
+                psw += alfabeto[g.Next(0, alfabeto.Length)];
+            return psw;
         }
+    }
+    public enum TipoPassword
+    {
+        Cifre, Lettere, CaratteriSpeciali
+    }
+    class GeneratorePasswordException : Exception
+    {
+        public GeneratorePasswordException(string messaggio) : base(messaggio)
+        { }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,15 @@
 ﻿using ClassiUtili;
 
+//for (int i = 33; i < 126; i++)
+//{
+//    Console.WriteLine($"{i}\t{(char)i}");
+//}
 
-GeneratorePassword g = new() { LunghezzaMinima = 6 };
+GeneratorePassword g1 = new(TipoPassword.Cifre) { LunghezzaMinima = 6 };
+GeneratorePassword g2 = new(TipoPassword.Lettere) { LunghezzaMinima = 6};
+GeneratorePassword g3 = new(TipoPassword.CaratteriSpeciali) { LunghezzaMinima = 6 };
 
-Console.WriteLine(g.NuovaPassword(12));
+
+Console.WriteLine(g1.Alfabeto);
+Console.WriteLine(g2.Alfabeto);
+Console.WriteLine(g3.Alfabeto);
