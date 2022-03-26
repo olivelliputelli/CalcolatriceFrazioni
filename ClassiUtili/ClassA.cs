9fcfdcf69ff1b398199a8516886767a5bb65e0ed
Davide Primino <dprimino@olivelliputelli.it>
2022-03-26 11:16:43 +0100

    Terminazione progetto "Generatore Password"

--- ClassiUtili/ClassA.cs
+++ ClassiUtili/ClassA.cs
@@ -1,12 +1,47 @@
 ﻿
 namespace ClassiUtili
 {
-    public static class ClassA
+    public class A
     {
-        public static int NumeroDiOccorrenzeCreate { get; set; } = 0;
-        static ClassA()
+        public static int P1 { get; set; } = 0;
+        private static int n1;
+        private static string n2 = "";
+        public static int N1
         {
-            NumeroDiOccorrenzeCreate++;
+            get { return n1; }
+            set { n1 = value; }
+        }
+        static A()
+        {
+            n2 = File.ReadAllText("");
+        }
+        public static int M1() => 5 + P1;
+
+    }
+
+    public class ClassA
+    {
+        public int P1 { get; set; } = 1;
+        private int p2 = 2;
+        protected int P3 { get; set; } = 3;
+        public ClassA(int p1) => P1 = p1;
+        public void M1() => Console.WriteLine("M1");
+    }
+
+    class ClassB : ClassA
+    {
+        private int n1;
+
+        public int N1
+        {
+            get { return n1; }
+            set { n1 = value; }
+        }
+
+        public ClassB(int p1, int n1) : base(p1)
+        {
+            N1 = n1;
         }
     }
+
 }
