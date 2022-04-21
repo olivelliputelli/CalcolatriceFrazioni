c8b2334da9a652c69bfa86c20ea42c7a30544285
Davide Primino <dprimino@olivelliputelli.it>
2022-04-21 12:42:42 +0200

    bitwise

---
+++ ClassiUtili/Paziente.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace ClassiUtili
+{
+    public class Paziente
+    {
+        int PazienteId { get; set; } 
+        public string Nome { get; set; } = "";
+        public DateTime? DataDiNascita { get; set; }
+    }
+}
