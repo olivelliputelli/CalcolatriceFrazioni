4e61ef7f04875f02b6cd141a3c7a68b1beb74d18
Davide Primino <dprimino@olivelliputelli.it>
2022-04-23 11:30:04 +0200

    Es Enum

---
+++ ClassiUtili/Enumerativi.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ClassiUtili
+{
+    [Flags]
+    public enum Stagione
+    {
+        Nessuna = 0b_0000,
+        Primavera = 0b_0001, // 1
+        Estate = 0b_0010, // 2
+        Autunno = 0b_0100, 
+        Inverno = 0b_1000,
+        AutunnoInverno = Autunno | Inverno
+    }
+    public enum Genere { Maschio, Femmina, Fluido, Altro }
+    public enum Sesso { Maschio, Femmina }
+
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,18 @@
 ﻿
+using ClassiUtili;
+
+
+//Console.Write("Stagione Preferita: ");
+//Stagione stagionePreferita = 
+//    (Stagione)Enum.Parse(typeof(Stagione), Console.ReadLine());
+
+Stagione adattoA = Stagione.Primavera | Stagione.Estate;
+
+Console.WriteLine(adattoA);
+
+
+
+
 
 
 
